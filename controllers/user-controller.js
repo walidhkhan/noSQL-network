b1b8387eb65e22c325feec79f9204dee17bff0b3
walidhkhan <walidhkhan@gmail.com>
2021-10-10 17:01:33 -0700

    updated user-controller to include post to add a new friend to users friend list & delete to remove friend from users friend list

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,4 +1,4 @@
-const { User } = require('../models');
+const { User, Thought } = require('../models');
 
 const userController = {
     // get all users
@@ -50,14 +50,58 @@ const userController = {
     },
 
     // delete user
-    deleteUser(req,res) {
-        User.findOneAndDelete({_id: req.params.id})
-        .then(dbUserData => res.json(dbUserData))
-        .catch(err => {
-            console.log(err);
-            res.status(500).json(err);
-        });
+    deleteUser(req, res) {
+        User.findOneAndDelete({ _id: req.params.id })
+            .then(dbUserData => res.json(dbUserData))
+            .catch(err => {
+                console.log(err);
+                res.status(500).json(err);
+            });
+    },
+
+    // add friend
+    addFriend({ params }, res) {
+        // add friendId to userIds friend list
+        User.findOneAndUpdate(
+            { _id: params.userId },
+            { $addToSet: { friends: params.friendId } },
+            { new: true, runValidators: true }
+        )
+            .then(dbUserData => res.json(dbUserData))
+        // add userId to friendId's friend list
+        User.findOneAndUpdate(
+            { _id: params.friendId },
+            { $addToSet: { friends: params.userId } },
+            { new: true, runValidators: true })
+            .then(dbUserData2 => res.json(dbUserData2))
+            .catch(err => {
+                console.log(err);
+                res.status(500).json(err);
+            });
+    },
+
+    // delete friend
+    deleteFriend({ params }, res) {
+        // delete friendId from userId's friend list
+        User.findOneAndUpdate(
+            { _id: params.userId },
+            { $pull: { friends: params.friendId } },
+            { new: true, runValidators: true }
+        )
+            .then(dbUserData => res.json(dbUserData))
+        // delete userId from friendId's friend list
+        User.findOneAndUpdate(
+            { _id: params.friendId },
+            { $pull: { friends: params.userId } },
+            { new: true, runValidators: true }
+        )
+            .then(dbUserData2 => res.json(dbUserData2))
+            .catch(err => {
+                console.log(err);
+                res.status(500).json(err);
+            });
     }
+
 };
 
 module.exports = userController;
